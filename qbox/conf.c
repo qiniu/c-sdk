e119ad62807f49bca42d45bdd797bc364308211d
fangdong <fangdong@pipul.org>
2012-08-16 12:48:22 +0800

    fixbug

--- qbox/conf.c
+++ qbox/conf.c
@@ -15,38 +15,14 @@ int QBOX_PUT_RETRY_TIMES				= 2;
 
 
 
-/*
-const char* QBOX_IO_HOST				= "http://iovip.qbox.me";
-const char* QBOX_FS_HOST				= "https://fs.qbox.me";
-const char* QBOX_RS_HOST				= "http://rs.qbox.me:10100";
-const char* QBOX_UP_HOST				= "http://up.qbox.me";
-const char* QBOX_PU_HOST				= "http://pu.qbox.me:10200";
-const char* QBOX_WM_HOST				= "http://m1.qbox.me:15000";
-*/
-
-#define QBOX_SDK_DEBUG
-
-#ifndef QBOX_SDK_DEBUG
 
 const char* QBOX_IO_HOST				= "http://iovip.qbox.me";
 const char* QBOX_FS_HOST				= "https://fs.qbox.me";
 const char* QBOX_RS_HOST				= "http://rs.qbox.me:10100";
 const char* QBOX_UP_HOST				= "http://up.qbox.me";
 const char* QBOX_PU_HOST				= "http://pu.qbox.me:10200";
-const char* QBOX_EU_HOST				= "http://m1.qbox.me:15000";
+const char* QBOX_EU_HOST				= "http://eu.qbox.me";
 
-#else
-
-const char* QBOX_IO_HOST				= "http://m1.qbox.me:13004";
-const char* QBOX_UC_HOST				= "http://m1.qbox.me:13010";
-const char* QBOX_FS_HOST				= "http://m1.qbox.me:13002";
-const char* QBOX_RS_HOST				= "http://m1.qbox.me:13003";
-const char* QBOX_UP_HOST				= "http://m1.qbox.me:13019";
-const char* QBOX_PU_HOST				= "http://m1.qbox.me:13012";
-const char* QBOX_AC_HOST				= "http://m1.qbox.me:13001";
-const char* QBOX_EU_HOST				= "http://m1.qbox.me:15000";
-
-#endif
 
 /*============================================================================*/
 
