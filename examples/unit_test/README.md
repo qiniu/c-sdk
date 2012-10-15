fc4516cff8f7c61075c8d1f181bb21868de52aa5
carter2000 <zarcardfly@gmail.com>
2012-10-15 18:31:21 +0800

    add imagemogrify,save-as

---
+++ examples/unit_test/README.md
@@ -0,0 +1,4 @@
+编译：make
+运行：./run_test
+
+注意：编译之前需要确认run_test.c中的QBOX_ACCESS_KEY和QBOX_SECRET_KEY正确。
---
+++ examples/unit_test/mogrifyurl.c
@@ -0,0 +1,10 @@
+#include "qbox_test.h"
+#include "../../qbox/image.h"
+
+int mogrifyurl(QBox_Client*)
+{
+	QBox_IMG_MogrOpts opts;
+	QBox_IMG_InitMogrOpts(&opts);
+	return 0;
+}
+
