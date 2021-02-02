5fc8e841cc9894234c0c8e4549a058925a814572
zengliang <zengliang@qiniu.com>
2021-02-02 15:40:12 +0800

    test commit

--- qiniu/rsf.c
+++ qiniu/rsf.c
@@ -12,13 +12,15 @@
 #include "../cJSON/cJSON.h"
 
 Qiniu_Error Qiniu_RSF_ListFiles(Qiniu_Client *self, Qiniu_RSF_ListRet *ret, const char *bucket, const char *prefix,
-                                const char *delimiter, const char *marker, int limit) {
+                                const char *delimiter, const char *marker, int limit)
+{
     Qiniu_Error err;
     int i;
     int prefixCount;
     int itemCount;
 
-    if (limit < 1 || limit > 1000) {
+    if (limit < 1 || limit > 1000)
+    {
         err.code = 400;
         err.message = "invalid list limit";
         return err;
@@ -39,26 +41,35 @@ Qiniu_Error Qiniu_RSF_ListFiles(Qiniu_Client *self, Qiniu_RSF_ListRet *ret, cons
 
     encodedBucket = Qiniu_QueryEscape(bucket, &escapeBucketOk);
 
-    if (prefix) {
+    if (prefix)
+    {
         encodedPrefix = Qiniu_QueryEscape(prefix, &escapePrefixOk);
-    } else {
+    }
+    else
+    {
         encodedPrefix = "";
     }
 
-    if (marker) {
+    if (marker)
+    {
         encodedMarker = Qiniu_QueryEscape(marker, &escapeMarkerOk);
-    } else {
+    }
+    else
+    {
         encodedMarker = "";
     }
 
-    if (delimiter) {
+    if (delimiter)
+    {
         encodedDelimiter = Qiniu_QueryEscape(delimiter, &escapeDelimiterOk);
-    } else {
+    }
+    else
+    {
         encodedDelimiter = "";
     }
-
+    //use atoi(limit) ?
     size_t limitLen = snprintf(NULL, 0, "%d", limit) + 1;
-    char *limitStr = (char *) malloc(sizeof(char) * limitLen);
+    char *limitStr = (char *)malloc(sizeof(char) * limitLen);
     memset(limitStr, 0, limitLen);
     snprintf(limitStr, limitLen, "%d", limit);
 
@@ -67,58 +78,72 @@ Qiniu_Error Qiniu_RSF_ListFiles(Qiniu_Client *self, Qiniu_RSF_ListRet *ret, cons
 
     Qiniu_Free(limitStr);
 
-    if (escapeBucketOk) {
+    if (escapeBucketOk)
+    {
         Qiniu_Free(encodedBucket);
     }
 
-    if (prefix && escapePrefixOk) {
+    if (prefix && escapePrefixOk)
+    {
         Qiniu_Free(encodedPrefix);
     }
 
-    if (delimiter && escapeDelimiterOk) {
+    if (delimiter && escapeDelimiterOk)
+    {
         Qiniu_Free(encodedDelimiter);
     }
 
-    if (marker && escapeMarkerOk) {
+    if (marker && escapeMarkerOk)
+    {
         Qiniu_Free(encodedMarker);
     }
 
     err = Qiniu_Client_Call(self, &root, url);
     Qiniu_Free(url);
-    if (err.code == 200) {
+    if (err.code == 200)
+    {
         ret->marker = Qiniu_Json_GetString(root, "marker", NULL);
         //check common prefixes
         prefixCount = Qiniu_Json_GetArraySize(root, "commonPrefixes", 0);
-        if (prefixCount > 0) {
+        if (prefixCount > 0)
+        {
             ret->commonPrefixesCount = prefixCount;
-            ret->commonPrefixes = (char **) malloc(sizeof(char **) * prefixCount);
+            ret->commonPrefixes = (char **)malloc(sizeof(char **) * prefixCount);
             char **p = ret->commonPrefixes;
             Qiniu_Json *commonPrefixes = Qiniu_Json_GetObjectItem(root, "commonPrefixes", NULL);
-            for (i = 0; i < prefixCount; i++) {
+            for (i = 0; i < prefixCount; i++)
+            {
                 *p = Qiniu_Json_GetArrayItem(commonPrefixes, i, NULL)->valuestring;
                 ++p;
             }
-        } else {
+        }
+        else
+        {
             ret->commonPrefixes = NULL;
             ret->commonPrefixesCount = 0;
         }
         //check items
         itemCount = Qiniu_Json_GetArraySize(root, "items", 0);
-        if (itemCount > 0) {
+        if (itemCount > 0)
+        {
             ret->itemsCount = itemCount;
-            ret->items = (Qiniu_RSF_ListItem *) malloc(sizeof(Qiniu_RSF_ListItem) * itemCount);
+            ret->items = (Qiniu_RSF_ListItem *)malloc(sizeof(Qiniu_RSF_ListItem) * itemCount);
 
             Qiniu_Json *items = Qiniu_Json_GetObjectItem(root, "items", NULL);
             Qiniu_RSF_ListItem *nextItem = 0;
-            for (i = 0; i < itemCount; i++) {
+            for (i = 0; i < itemCount; i++)
+            {
                 Qiniu_Json *itemObj = Qiniu_Json_GetArrayItem(items, i, NULL);
                 Qiniu_RSF_ListItem item;
                 item.key = Qiniu_Json_GetString(itemObj, "key", NULL);
                 item.hash = Qiniu_Json_GetString(itemObj, "hash", NULL);
                 item.mimeType = Qiniu_Json_GetString(itemObj, "mimeType", NULL);
-                if (Qiniu_Json_GetString(itemObj, "endUser", NULL) != 0) {
+                if (Qiniu_Json_GetString(itemObj, "endUser", NULL) != 0)
+                {
                     item.endUser = Qiniu_Json_GetString(itemObj, "endUser", NULL);
-                } else {
+                }
+                else
+                {
                     item.endUser = NULL;
                 }
                 item.putTime = Qiniu_Json_GetInt64(itemObj, "putTime", 0);
@@ -127,7 +152,9 @@ Qiniu_Error Qiniu_RSF_ListFiles(Qiniu_Client *self, Qiniu_RSF_ListRet *ret, cons
 
                 ret->items[i] = item;
             }
-        } else {
+        }
+        else
+        {
             ret->items = NULL;
             ret->itemsCount = 0;
         }
