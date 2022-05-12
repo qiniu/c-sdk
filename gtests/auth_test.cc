dd4229d8dd33a715b6fb43468ed74bb127573f8a
Bachue Zhou <bachue.shu@gmail.com>
2022-05-12 09:42:37 +0800

    add timestamp signature

--- gtests/auth_test.cc
+++ gtests/auth_test.cc
@@ -3,12 +3,15 @@
 #include "test.h"
 #include "qiniu/http.h"
 
-static Qiniu_Bool match_headers(Qiniu_Header* header, const char* expected_header) {
-	for (Qiniu_Header *cur_header = header; cur_header != NULL; cur_header = cur_header->next) {
-        if (strcmp(cur_header->data, expected_header) == 0) {
+static Qiniu_Bool match_header_prefix(Qiniu_Header *header, const char *expected_header)
+{
+    for (Qiniu_Header *cur_header = header; cur_header != NULL; cur_header = cur_header->next)
+    {
+        if (strncmp(cur_header->data, expected_header, strlen(expected_header)) == 0)
+        {
             return Qiniu_True;
         }
-	}
+    }
     return Qiniu_False;
 }
 
@@ -17,14 +20,30 @@ TEST(UnitTest, TestQiniuMacAuthV2)
     Qiniu_Mac mac = {"ak", "sk"};
     Qiniu_Auth auth = Qiniu_MacAuth(&mac);
     Qiniu_Error err;
-    Qiniu_Header* header = NULL;
+    Qiniu_Header *header = NULL;
     header = curl_slist_append(header, "X-Qiniu-: a");
     header = curl_slist_append(header, "X-Qiniu: b");
     header = curl_slist_append(header, "Content-Type: application/x-www-form-urlencoded");
     const char *body = "{\"name\": \"test\"}";
+    Qiniu_MacAuth_Enable_Qiniu_Timestamp_Signature();
     err = auth.itbl->AuthV2(&mac, "GET", &header, "http://upload.qiniup.com/", body, strlen(body));
     EXPECT_EQ(err.code, 200);
-    match_headers(header, "Authorization: Qiniu ak:156x8Q4x1zadPcAyMRVDsioIyAk=");
+    EXPECT_EQ(match_header_prefix(header, "Authorization: Qiniu ak:"), Qiniu_True);
+    EXPECT_EQ(match_header_prefix(header, "X-Qiniu-Date:"), Qiniu_True);
+    curl_slist_free_all(header);
+
+    header = NULL;
+    Qiniu_MacAuth_Disable_Qiniu_Timestamp_Signature();
+    err = auth.itbl->AuthV2(&mac, "GET", &header, "http://upload.qiniup.com/", body, strlen(body));
+    Qiniu_MacAuth_Enable_Qiniu_Timestamp_Signature();
+    EXPECT_EQ(err.code, 200);
+    EXPECT_EQ(match_header_prefix(header, "X-Qiniu-Date:"), Qiniu_False);
+    curl_slist_free_all(header);
+
+    header = NULL;
+    err = auth.itbl->AuthV2(&mac, "GET", &header, "http://upload.qiniup.com/", body, strlen(body));
+    EXPECT_EQ(err.code, 200);
+    EXPECT_EQ(match_header_prefix(header, "X-Qiniu-Date:"), Qiniu_True);
     curl_slist_free_all(header);
 
     header = NULL;
@@ -33,12 +52,18 @@ TEST(UnitTest, TestQiniuMacAuthV2)
     header = curl_slist_append(header, "X-Qiniu-Bbb: BBB");
     header = curl_slist_append(header, "X-Qiniu-Aaa: DDD");
     body = "name=test&language=go}";
+    Qiniu_MacAuth_Disable_Qiniu_Timestamp_Signature();
     err = auth.itbl->AuthV2(&mac, "GET", &header, "http://upload.qiniup.com/mkfile/sdf.jpg", body, strlen(body));
+    Qiniu_MacAuth_Enable_Qiniu_Timestamp_Signature();
     EXPECT_EQ(err.code, 200);
-    match_headers(header, "Authorization: Qiniu ak:K8d62cW_hqjxQ3RElNz8g3BQHa8=");
+    match_header_prefix(header, "Authorization: Qiniu ak:K8d62cW_hqjxQ3RElNz8g3BQHa8=");
+    EXPECT_EQ(match_header_prefix(header, "X-Qiniu-Date:"), Qiniu_False);
 
+    Qiniu_MacAuth_Disable_Qiniu_Timestamp_Signature();
     err = auth.itbl->AuthV2(&mac, "GET", &header, "http://upload.qiniup.com/mkfile/sdf.jpg?s=er3&df", body, strlen(body));
+    Qiniu_MacAuth_Enable_Qiniu_Timestamp_Signature();
     EXPECT_EQ(err.code, 200);
-    match_headers(header, "Authorization: Qiniu ak:CzOiB_NSxrvMLkhK8hhV_1vTqYk=");
+    match_header_prefix(header, "Authorization: Qiniu ak:CzOiB_NSxrvMLkhK8hhV_1vTqYk=");
+    EXPECT_EQ(match_header_prefix(header, "X-Qiniu-Date:"), Qiniu_False);
     curl_slist_free_all(header);
 }
--- qiniu/auth_mac.c
+++ qiniu/auth_mac.c
@@ -8,6 +8,7 @@
  */
 
 #include "http.h"
+#include <time.h>
 #include <ctype.h>
 #include <curl/curl.h>
 #include <openssl/hmac.h>
@@ -18,8 +19,22 @@
 #pragma comment(lib, "libcrypto.lib")
 #define strcasecmp _stricmp
 #define strncasecmp _strnicmp
+#define _setenv _putenv_s
+static errno_t gmtime_r(const time_t *sourceTime, struct tm *tmDest)
+{
+	return gmtime_s(tmDest, sourceTime);
+}
+static errno_t _unsetenv(const char *varname)
+{
+	return _putenv_s(varname, "");
+}
 #else
 #define Qiniu_Posix_strndup strndup
+#define _unsetenv unsetenv
+static int _setenv(const char *name, const char *value)
+{
+	return setenv(name, value, 1);
+}
 #endif
 
 /*============================================================================*/
@@ -46,10 +61,23 @@ void Qiniu_Servend_Cleanup()
 	Qiniu_Global_Cleanup();
 }
 
+static const char *DISABLE_QINIU_TIMESTAMP_SIGNATURE = "DISABLE_QINIU_TIMESTAMP_SIGNATURE";
+
+void Qiniu_MacAuth_Enable_Qiniu_Timestamp_Signature()
+{
+	_unsetenv(DISABLE_QINIU_TIMESTAMP_SIGNATURE);
+}
+
+void Qiniu_MacAuth_Disable_Qiniu_Timestamp_Signature()
+{
+	_setenv(DISABLE_QINIU_TIMESTAMP_SIGNATURE, "1");
+}
+
 /*============================================================================*/
 /* type Qiniu_Mac */
 
-static void Qiniu_Mac_Hmac_inner(Qiniu_Mac *mac, const char *items[], size_t items_len, const char *addition, size_t addlen, char *digest, unsigned int *digest_len)
+static void
+Qiniu_Mac_Hmac_inner(Qiniu_Mac *mac, const char *items[], size_t items_len, const char *addition, size_t addlen, char *digest, unsigned int *digest_len)
 {
 #if OPENSSL_VERSION_NUMBER < 0x10100000
 	HMAC_CTX ctx;
@@ -92,7 +120,8 @@ static void Qiniu_Mac_Hmac(Qiniu_Mac *mac, const char *path, const char *additio
 	Qiniu_Mac_Hmac_inner(mac, items, sizeof(items) / sizeof(const char *), addition, addlen, digest, digest_len);
 }
 
-typedef struct Qiniu_Found_X_Qiniu_Header {
+typedef struct Qiniu_Found_X_Qiniu_Header
+{
 	char *header_name;
 	char *header_value;
 	char *free_name_ptr;
@@ -101,26 +130,30 @@ typedef struct Qiniu_Found_X_Qiniu_Header {
 
 static void Qiniu_Mac_HmacV2(
 	Qiniu_Mac *mac, const char *method, const char *host, const char *path, const char *contentType,
-	Qiniu_Found_X_Qiniu_Header* x_header, int x_header_count,
+	Qiniu_Found_X_Qiniu_Header *x_header, int x_header_count,
 	const char *addition, size_t addlen, char *digest, unsigned int *digest_len)
 {
 	const char *items[] = {method, " ", path, "\nHost: ", host, "\nContent-Type: ", contentType, "\n"};
-	if (x_header == NULL) {
+	if (x_header == NULL)
+	{
 		size_t count = sizeof(items) / sizeof(const char *);
 		Qiniu_Mac_Hmac_inner(mac, items, count, addition, addlen, digest, digest_len);
-	} else {
+	}
+	else
+	{
 		size_t new_items_offset = sizeof(items) / sizeof(const char *);
 		size_t new_items_count = new_items_offset + x_header_count * 4;
-		const char **new_items = (const char **) malloc(new_items_count * sizeof(char *));
+		const char **new_items = (const char **)malloc(new_items_count * sizeof(char *));
 		memcpy(new_items, items, sizeof(items));
-		for (size_t i = 0; i < x_header_count; i++) {
+		for (size_t i = 0; i < x_header_count; i++)
+		{
 			new_items[new_items_offset++] = x_header[i].header_name;
 			new_items[new_items_offset++] = ": ";
 			new_items[new_items_offset++] = x_header[i].header_value;
 			new_items[new_items_offset++] = "\n";
 		}
 		Qiniu_Mac_Hmac_inner(mac, new_items, new_items_count, addition, addlen, digest, digest_len);
-        Qiniu_Free(new_items);
+		Qiniu_Free(new_items);
 	}
 }
 
@@ -205,46 +238,56 @@ static Qiniu_Error Qiniu_Mac_Auth(
 #define APPLICATION_OCTET_STREAM "application/octet-stream"
 #define APPLICATION_WWW_FORM_URLENCODED "application/x-www-form-urlencoded"
 
-static Qiniu_Error Qiniu_For_Each_Header(Qiniu_Header *header, Qiniu_Error (*for_each)(Qiniu_Header *header, void *data), void *data) {
-	for (Qiniu_Header *cur_header = header; cur_header != NULL; cur_header = cur_header->next) {
+static Qiniu_Error Qiniu_For_Each_Header(Qiniu_Header *header, Qiniu_Error (*for_each)(Qiniu_Header *header, void *data), void *data)
+{
+	for (Qiniu_Header *cur_header = header; cur_header != NULL; cur_header = cur_header->next)
+	{
 		Qiniu_Error err = for_each(cur_header, data);
-		if (err.code != 200) {
+		if (err.code != 200)
+		{
 			return err;
 		}
 	}
 	return Qiniu_OK;
 }
 
-static char* trim_string(char *str)
+static char *trim_string(char *str)
 {
-    char *end;
+	char *end;
 
-    while(isspace((unsigned char)*str)) str++;
+	while (isspace((unsigned char)*str))
+		str++;
 
-    if (*str == '\0') {
-        return str;
-    }
+	if (*str == '\0')
+	{
+		return str;
+	}
 
-    end = str + strlen(str) - 1;
-    while(end > str && isspace((unsigned char)*end)) end--;
-    end[1] = '\0';
+	end = str + strlen(str) - 1;
+	while (end > str && isspace((unsigned char)*end))
+		end--;
+	end[1] = '\0';
 
-    return str;
+	return str;
 }
 
-typedef struct Qiniu_Find_Content_Type_Callback_Data {
+typedef struct Qiniu_Find_Content_Type_Callback_Data
+{
 	char *found_content_type;
 	char *free_ptr;
 } Qiniu_Find_Content_Type_Callback_Data;
 
-static Qiniu_Error Qiniu_Find_Content_Type_Callback(Qiniu_Header *header, void *data) {
+static Qiniu_Error Qiniu_Find_Content_Type_Callback(Qiniu_Header *header, void *data)
+{
 	Qiniu_Find_Content_Type_Callback_Data *callback_data = (Qiniu_Find_Content_Type_Callback_Data *)data;
 	const char *colonPos = strchr(header->data, ':');
-	if (colonPos != NULL) {
-		if (strncasecmp(header->data, "Content-Type", colonPos - header->data) == 0) {
+	if (colonPos != NULL)
+	{
+		if (strncasecmp(header->data, "Content-Type", colonPos - header->data) == 0)
+		{
 			callback_data->free_ptr = strdup(colonPos + 1);
 			callback_data->found_content_type = trim_string(callback_data->free_ptr);
-			Qiniu_Error found = { 201, "Found" };
+			Qiniu_Error found = {201, "Found"};
 			return found;
 		}
 	}
@@ -252,17 +295,22 @@ static Qiniu_Error Qiniu_Find_Content_Type_Callback(Qiniu_Header *header, void *
 	return Qiniu_OK;
 }
 
-static Qiniu_Bool is_x_qiniu_header(Qiniu_Header *header) {
+static Qiniu_Bool is_x_qiniu_header(Qiniu_Header *header)
+{
 	const char *x_qiniu_header_prefix = "X-Qiniu-";
 	const char *colonPos = strchr(header->data, ':');
-	if (colonPos != NULL) {
-		if (strncasecmp(header->data, x_qiniu_header_prefix, strlen(x_qiniu_header_prefix)) == 0) {
+	if (colonPos != NULL)
+	{
+		if (strncasecmp(header->data, x_qiniu_header_prefix, strlen(x_qiniu_header_prefix)) == 0)
+		{
 			char *first_space_in_header = strchr(header->data, ' ');
 			int header_key_length = colonPos - header->data;
-			if (first_space_in_header != NULL && first_space_in_header < colonPos) {
+			if (first_space_in_header != NULL && first_space_in_header < colonPos)
+			{
 				header_key_length = first_space_in_header - header->data;
 			}
-			if (header_key_length > strlen(x_qiniu_header_prefix)) {
+			if (header_key_length > strlen(x_qiniu_header_prefix))
+			{
 				return Qiniu_True;
 			}
 		}
@@ -270,12 +318,15 @@ static Qiniu_Bool is_x_qiniu_header(Qiniu_Header *header) {
 	return Qiniu_False;
 }
 
-typedef struct Qiniu_Count_X_Qiniu_Callback_Data {
+typedef struct Qiniu_Count_X_Qiniu_Callback_Data
+{
 	size_t count;
 } Qiniu_Count_X_Qiniu_Callback_Data;
 
-static Qiniu_Error Qiniu_Count_X_Qiniu_Callback(Qiniu_Header *header, void *data) {
-	if (is_x_qiniu_header(header)) {
+static Qiniu_Error Qiniu_Count_X_Qiniu_Callback(Qiniu_Header *header, void *data)
+{
+	if (is_x_qiniu_header(header))
+	{
 		Qiniu_Count_X_Qiniu_Callback_Data *callback_data = (Qiniu_Count_X_Qiniu_Callback_Data *)data;
 		callback_data->count++;
 	}
@@ -285,41 +336,51 @@ static Qiniu_Error Qiniu_Count_X_Qiniu_Callback(Qiniu_Header *header, void *data
 #define VALID_TOKEN_TABLE_SIZE (127)
 static Qiniu_Bool valid_token_table[VALID_TOKEN_TABLE_SIZE] = {Qiniu_False};
 
-static Qiniu_Bool* initialize_token_table() {
+static Qiniu_Bool *initialize_token_table()
+{
 	static Qiniu_Bool is_initialized = Qiniu_False;
 	static char tokens[] = {
 		'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
 		'U', 'W', 'V', 'X', 'Y', 'Z', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-		'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '|', '~'
-	};
-	if (is_initialized == Qiniu_False) {
+		'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '|', '~'};
+	if (is_initialized == Qiniu_False)
+	{
 		is_initialized = Qiniu_True;
-		for (int i = 0; i < sizeof(tokens) / sizeof(char); i++) {
+		for (int i = 0; i < sizeof(tokens) / sizeof(char); i++)
+		{
 			valid_token_table[i] = Qiniu_True;
 		}
 	}
 	return valid_token_table;
 }
 
-static Qiniu_Bool is_valid_header_field_byte(char c, Qiniu_Bool* valid_token_table) {
+static Qiniu_Bool is_valid_header_field_byte(char c, Qiniu_Bool *valid_token_table)
+{
 	return 0 < c && c < VALID_TOKEN_TABLE_SIZE && valid_token_table[(int)c];
 }
 
-static void canonical_mime_header_key(char *name) {
+static void canonical_mime_header_key(char *name)
+{
 	static const char TO_LOWER = 'a' - 'A';
 	Qiniu_Bool *valid_token_table = initialize_token_table();
-	for (size_t i = 0; i < strlen(name); i++) {
-		if (is_valid_header_field_byte(name[i], valid_token_table) == Qiniu_False) {
+	for (size_t i = 0; i < strlen(name); i++)
+	{
+		if (is_valid_header_field_byte(name[i], valid_token_table) == Qiniu_False)
+		{
 			return;
 		}
 	}
 	Qiniu_Bool upper = Qiniu_True;
-	for (size_t i = 0; i < strlen(name); i++) {
+	for (size_t i = 0; i < strlen(name); i++)
+	{
 		char c = name[i];
-		if (upper && 'a' <= c && c <= 'z') {
+		if (upper && 'a' <= c && c <= 'z')
+		{
 			c -= TO_LOWER;
-		} else if (!upper && 'A' <= c && c <= 'Z') {
+		}
+		else if (!upper && 'A' <= c && c <= 'Z')
+		{
 			c += TO_LOWER;
 		}
 		name[i] = c;
@@ -328,26 +389,30 @@ static void canonical_mime_header_key(char *name) {
 	return;
 }
 
-static int compare_x_qiniu_header(const void *left, const void *right) {
+static int compare_x_qiniu_header(const void *left, const void *right)
+{
 	Qiniu_Found_X_Qiniu_Header *left_header = (Qiniu_Found_X_Qiniu_Header *)left;
 	Qiniu_Found_X_Qiniu_Header *right_header = (Qiniu_Found_X_Qiniu_Header *)right;
 	return strcmp(left_header->header_name, right_header->header_name);
 }
 
-typedef struct Qiniu_Find_X_Qiniu_Callback_Data {
+typedef struct Qiniu_Find_X_Qiniu_Callback_Data
+{
 	Qiniu_Found_X_Qiniu_Header *found_x_qiniu_headers;
 	size_t index;
 } Qiniu_Find_X_Qiniu_Callback_Data;
 
-static Qiniu_Error Qiniu_Find_X_Qiniu_Callback(Qiniu_Header *header, void *data) {
-	if (is_x_qiniu_header(header)) {
+static Qiniu_Error Qiniu_Find_X_Qiniu_Callback(Qiniu_Header *header, void *data)
+{
+	if (is_x_qiniu_header(header))
+	{
 		const char *colonPos = strchr(header->data, ':');
-		if (colonPos != NULL) {
+		if (colonPos != NULL)
+		{
 			Qiniu_Find_X_Qiniu_Callback_Data *callback_data = (Qiniu_Find_X_Qiniu_Callback_Data *)data;
 			Qiniu_Found_X_Qiniu_Header pair = {
 				.free_name_ptr = Qiniu_Posix_strndup(header->data, colonPos - header->data),
-				.free_value_ptr = strdup(colonPos + 1)
-			};
+				.free_value_ptr = strdup(colonPos + 1)};
 			pair.header_name = trim_string(pair.free_name_ptr);
 			pair.header_value = trim_string(pair.free_value_ptr);
 			canonical_mime_header_key(pair.header_name);
@@ -358,7 +423,10 @@ static Qiniu_Error Qiniu_Find_X_Qiniu_Callback(Qiniu_Header *header, void *data)
 	return Qiniu_OK;
 }
 
-static Qiniu_Error Qiniu_Mac_AuthV2(
+static const char *X_QINIU_DATE = "X-Qiniu-Date: ";
+
+static Qiniu_Error
+Qiniu_Mac_AuthV2(
 	void *self, const char *method, Qiniu_Header **header, const char *url, const char *addition, size_t addlen)
 {
 	Qiniu_Error err;
@@ -397,9 +465,21 @@ static Qiniu_Error Qiniu_Mac_AuthV2(
 	{
 		addlen = 0;
 	}
-	if (find_content_type_callback_data.found_content_type == NULL) {
+	if (find_content_type_callback_data.found_content_type == NULL)
+	{
 		*header = curl_slist_append(*header, "Content-Type: " APPLICATION_WWW_FORM_URLENCODED);
 		find_content_type_callback_data.found_content_type = APPLICATION_WWW_FORM_URLENCODED;
+	}
+	if (getenv(DISABLE_QINIU_TIMESTAMP_SIGNATURE) == NULL)
+	{
+		time_t rawtime;
+		struct tm gmtime;
+		char time_buf[32] = {0};
+		strncpy(time_buf, X_QINIU_DATE, sizeof(time_buf));
+		time(&rawtime);
+		gmtime_r(&rawtime, &gmtime);
+		strftime(time_buf + strlen(X_QINIU_DATE), sizeof(time_buf) - strlen(X_QINIU_DATE), "%Y%m%dT%H%M%SZ", &gmtime);
+		*header = curl_slist_append(*header, time_buf);
 	}
 
 	Qiniu_Count_X_Qiniu_Callback_Data count_x_qiniu_callback_data = {0};
@@ -407,8 +487,9 @@ static Qiniu_Error Qiniu_Mac_AuthV2(
 
 	Qiniu_Find_X_Qiniu_Callback_Data find_x_qiniu_callback_data = {NULL, 0};
 
-	if (count_x_qiniu_callback_data.count > 0) {
-		find_x_qiniu_callback_data.found_x_qiniu_headers = (Qiniu_Found_X_Qiniu_Header*) malloc(count_x_qiniu_callback_data.count * sizeof(Qiniu_Found_X_Qiniu_Header));
+	if (count_x_qiniu_callback_data.count > 0)
+	{
+		find_x_qiniu_callback_data.found_x_qiniu_headers = (Qiniu_Found_X_Qiniu_Header *)malloc(count_x_qiniu_callback_data.count * sizeof(Qiniu_Found_X_Qiniu_Header));
 		Qiniu_For_Each_Header(*header, Qiniu_Find_X_Qiniu_Callback, &find_x_qiniu_callback_data);
 		qsort(find_x_qiniu_callback_data.found_x_qiniu_headers, count_x_qiniu_callback_data.count, sizeof(Qiniu_Found_X_Qiniu_Header), compare_x_qiniu_header);
 	}
@@ -419,15 +500,18 @@ static Qiniu_Error Qiniu_Mac_AuthV2(
 		find_x_qiniu_callback_data.found_x_qiniu_headers, count_x_qiniu_callback_data.count,
 		addition, addlen, digest, &digest_len);
 
-	if (find_content_type_callback_data.free_ptr != NULL) {
+	if (find_content_type_callback_data.free_ptr != NULL)
+	{
 		Qiniu_Free((void *)find_content_type_callback_data.free_ptr);
 	}
-	if (find_x_qiniu_callback_data.found_x_qiniu_headers != NULL) {
-		for (size_t i = 0; i < count_x_qiniu_callback_data.count; i++) {
+	if (find_x_qiniu_callback_data.found_x_qiniu_headers != NULL)
+	{
+		for (size_t i = 0; i < count_x_qiniu_callback_data.count; i++)
+		{
 			Qiniu_Free((void *)find_x_qiniu_callback_data.found_x_qiniu_headers[i].free_name_ptr);
 			Qiniu_Free((void *)find_x_qiniu_callback_data.found_x_qiniu_headers[i].free_value_ptr);
 		}
-        Qiniu_Free(find_x_qiniu_callback_data.found_x_qiniu_headers);
+		Qiniu_Free(find_x_qiniu_callback_data.found_x_qiniu_headers);
 	}
 	Qiniu_Free(normalizedHost);
 	enc_digest = Qiniu_Memory_Encode(digest, digest_len);
