0579b0668eef1d0e49071e4bfdfd2e619ef53a69
Rong Zhou <zhourong@qiniu.com>
2023-07-12 06:25:17 +0000

    remove Zone_North_East_1

--- qiniu/conf.c
+++ qiniu/conf.c
@@ -15,27 +15,30 @@ const char *version = "7.3.0";
 const char *QINIU_ACCESS_KEY = "<Please apply your access key>";
 const char *QINIU_SECRET_KEY = "<Dont send your secret key to anyone>";
 
-//公共服务http域名
+// 公共服务http域名
 const char *QINIU_RS_HOST = "http://rs.qbox.me";
 const char *QINIU_RSF_HOST = "http://rsf.qbox.me";
 const char *QINIU_API_HOST = "http://api.qiniu.com";
 const char *QINIU_FUSION_HOST = "http://fusion.qiniuapi.com";
 const char *QINIU_UC_HOST = "http://uc.qbox.me";
 
-//默认华东机房
+// 默认华东机房
 const char *QINIU_UP_HOST = "http://upload.qiniup.com";
 const char *QINIU_IOVIP_HOST = "http://iovip.qbox.me";
 
-
-//设置华东机房域名
-void Qiniu_Use_Zone_Huadong(Qiniu_Bool useHttps) {
-    if (useHttps == Qiniu_True) {
+// 设置华东机房域名
+void Qiniu_Use_Zone_Huadong(Qiniu_Bool useHttps)
+{
+    if (useHttps == Qiniu_True)
+    {
         QINIU_RS_HOST = "https://rs.qbox.me";
         QINIU_RSF_HOST = "https://rsf.qbox.me";
         QINIU_API_HOST = "https://api.qiniu.com";
         QINIU_UP_HOST = "https://upload.qiniup.com";
         QINIU_IOVIP_HOST = "https://iovip.qbox.me";
-    } else {
+    }
+    else
+    {
         QINIU_RS_HOST = "http://rs.qbox.me";
         QINIU_RSF_HOST = "http://rsf.qbox.me";
         QINIU_API_HOST = "http://api.qiniu.com";
@@ -44,15 +47,19 @@ void Qiniu_Use_Zone_Huadong(Qiniu_Bool useHttps) {
     }
 }
 
-//设置华北机房域名
-void Qiniu_Use_Zone_Huabei(Qiniu_Bool useHttps) {
-    if (useHttps == Qiniu_True) {
+// 设置华北机房域名
+void Qiniu_Use_Zone_Huabei(Qiniu_Bool useHttps)
+{
+    if (useHttps == Qiniu_True)
+    {
         QINIU_RS_HOST = "https://rs-z1.qbox.me";
         QINIU_RSF_HOST = "https://rsf-z1.qbox.me";
         QINIU_API_HOST = "https://api-z1.qiniu.com";
         QINIU_UP_HOST = "https://upload-z1.qiniup.com";
         QINIU_IOVIP_HOST = "https://iovip-z1.qbox.me";
-    } else {
+    }
+    else
+    {
         QINIU_RS_HOST = "http://rs-z1.qbox.me";
         QINIU_RSF_HOST = "http://rsf-z1.qbox.me";
         QINIU_API_HOST = "http://api-z1.qiniu.com";
@@ -61,15 +68,19 @@ void Qiniu_Use_Zone_Huabei(Qiniu_Bool useHttps) {
     }
 }
 
-//设置华南机房域名
-void Qiniu_Use_Zone_Huanan(Qiniu_Bool useHttps) {
-    if (useHttps == Qiniu_True) {
+// 设置华南机房域名
+void Qiniu_Use_Zone_Huanan(Qiniu_Bool useHttps)
+{
+    if (useHttps == Qiniu_True)
+    {
         QINIU_RS_HOST = "https://rs-z2.qbox.me";
         QINIU_RSF_HOST = "https://rsf-z2.qbox.me";
         QINIU_API_HOST = "https://api-z2.qiniu.com";
         QINIU_UP_HOST = "https://upload-z2.qiniup.com";
         QINIU_IOVIP_HOST = "https://iovip-z2.qbox.me";
-    } else {
+    }
+    else
+    {
         QINIU_RS_HOST = "http://rs-z2.qbox.me";
         QINIU_RSF_HOST = "http://rsf-z2.qbox.me";
         QINIU_API_HOST = "http://api-z2.qiniu.com";
@@ -78,15 +89,19 @@ void Qiniu_Use_Zone_Huanan(Qiniu_Bool useHttps) {
     }
 }
 
-//设置北美机房域名
-void Qiniu_Use_Zone_Beimei(Qiniu_Bool useHttps) {
-    if (useHttps == Qiniu_True) {
+// 设置北美机房域名
+void Qiniu_Use_Zone_Beimei(Qiniu_Bool useHttps)
+{
+    if (useHttps == Qiniu_True)
+    {
         QINIU_RS_HOST = "https://rs-na0.qbox.me";
         QINIU_RSF_HOST = "https://rsf-na0.qbox.me";
         QINIU_API_HOST = "https://api-na0.qiniu.com";
         QINIU_UP_HOST = "https://upload-na0.qiniup.com";
         QINIU_IOVIP_HOST = "https://iovip-na0.qbox.me";
-    } else {
+    }
+    else
+    {
         QINIU_RS_HOST = "http://rs-na0.qbox.me";
         QINIU_RSF_HOST = "http://rsf-na0.qbox.me";
         QINIU_API_HOST = "http://api-na0.qiniu.com";
@@ -95,15 +110,19 @@ void Qiniu_Use_Zone_Beimei(Qiniu_Bool useHttps) {
     }
 }
 
-//设置新加坡机房
-void Qiniu_Use_Zone_Dongnanya(Qiniu_Bool useHttps) {
-    if (useHttps == Qiniu_True) {
+// 设置新加坡机房
+void Qiniu_Use_Zone_Dongnanya(Qiniu_Bool useHttps)
+{
+    if (useHttps == Qiniu_True)
+    {
         QINIU_RS_HOST = "https://rs-as0.qbox.me";
         QINIU_RSF_HOST = "https://rsf-as0.qbox.me";
         QINIU_API_HOST = "https://api-as0.qiniu.com";
         QINIU_UP_HOST = "https://upload-as0.qiniup.com";
         QINIU_IOVIP_HOST = "https://iovip-as0.qbox.me";
-    } else {
+    }
+    else
+    {
         QINIU_RS_HOST = "http://rs-as0.qbox.me";
         QINIU_RSF_HOST = "http://rsf-as0.qbox.me";
         QINIU_API_HOST = "http://api-as0.qiniu.com";
@@ -112,15 +131,19 @@ void Qiniu_Use_Zone_Dongnanya(Qiniu_Bool useHttps) {
     }
 }
 
-//设置华东二区机房
-void Qiniu_Use_Zone_Cn_East_2(Qiniu_Bool useHttps) {
-    if (useHttps == Qiniu_True) {
+// 设置华东二区机房
+void Qiniu_Use_Zone_Cn_East_2(Qiniu_Bool useHttps)
+{
+    if (useHttps == Qiniu_True)
+    {
         QINIU_RS_HOST = "https://rs-cn-east-2.qiniuapi.com";
         QINIU_RSF_HOST = "https://rsf-cn-east-2.qiniuapi.com";
         QINIU_API_HOST = "https://api-cn-east-2.qiniuapi.com";
         QINIU_UP_HOST = "https://upload-cn-east-2.qiniup.com";
         QINIU_IOVIP_HOST = "https://iovip-cn-east-2.qiniuio.com";
-    } else {
+    }
+    else
+    {
         QINIU_RS_HOST = "http://rs-cn-east-2.qiniuapi.com";
         QINIU_RSF_HOST = "http://rsf-cn-east-2.qiniuapi.com";
         QINIU_API_HOST = "http://api-cn-east-2.qiniuapi.com";
@@ -129,21 +152,4 @@ void Qiniu_Use_Zone_Cn_East_2(Qiniu_Bool useHttps) {
     }
 }
 
-//设置首尔机房
-void Qiniu_Use_Zone_North_East_1(Qiniu_Bool useHttps) {
-    if (useHttps == Qiniu_True) {
-        QINIU_RS_HOST = "https://rs-ap-northeast-1.qiniuapi.com";
-        QINIU_RSF_HOST = "https://rsf-ap-northeast-1.qiniuapi.com";
-        QINIU_API_HOST = "https://api-ap-northeast-1.qiniuapi.com";
-        QINIU_UP_HOST = "https://upload-ap-northeast-1.qiniup.com";
-        QINIU_IOVIP_HOST = "https://iovip-ap-northeast-1.qiniuio.com";
-    } else {
-        QINIU_RS_HOST = "http://rs-ap-northeast-1.qiniuapi.com";
-        QINIU_RSF_HOST = "http://rsf-ap-northeast-1.qiniuapi.com";
-        QINIU_API_HOST = "http://api-ap-northeast-1.qiniuapi.com";
-        QINIU_UP_HOST = "http://upload-ap-northeast-1.qiniup.com";
-        QINIU_IOVIP_HOST = "http://iovip-ap-northeast-1.qiniuio.com";
-    }
-}
 /*============================================================================*/
-
